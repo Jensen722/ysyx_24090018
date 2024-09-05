e9edfa0bcda6fd8d33cfe6a7d98736117b855511
tracer-ysyx <tracer@ysyx.org>
2024-09-05 12:07:24 +0800

    > compile NEMU ysyx_24040000 林建辉 Linux HP 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:07:24 up 12:20,  1 user,  load average: 0.62, 0.67, 0.65

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -125,6 +125,9 @@ void sdb_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if(strcmp(cmd, "q") == 0) {
+          nemu_state.state = NEMU_QUIT;
+        }
         if (cmd_table[i].handler(args) < 0) { return; }
         break;
       }
