fceabf68d61c9c77de4b4197cead6c178c3cad68
tracer-ysyx <tracer@ysyx.org>
2024-09-13 17:41:22 +0800

    > compile NEMU ysyx_24040000 林建辉 Linux HP 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:41:22 up 8 days, 17:54,  1 user,  load average: 0.15, 0.20, 0.24

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -107,9 +107,14 @@ void free_wp(int N){
 }
 
 void info_wp(){
-  printf("Num\t    Type\t Enb\t What\n");
-  for(WP *wp = head; wp; wp = wp->next){
-    printf("%2d\t watchpoint\t  %c\t %s\n", wp->NO, wp->Enb ? 'y' : 'n', wp->expr);
+  if(head != NULL){
+    printf("Num\t    Type\t Enb\t What\n");
+    for(WP *wp = head; wp; wp = wp->next){
+      printf("%2d\t watchpoint\t  %c\t %s\n", wp->NO, wp->Enb ? 'y' : 'n', wp->expr);
+    }
+  }
+  else {
+    printf("No watchpoint!\n");
   }
 }
 
