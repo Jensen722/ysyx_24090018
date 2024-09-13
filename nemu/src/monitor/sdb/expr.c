c96dd274cf690a1f64a0ac3b5c681ae626569186
tracer-ysyx <tracer@ysyx.org>
2024-09-13 22:02:09 +0800

    > compile NEMU ysyx_24040000 林建辉 Linux HP 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:02:09 up 8 days, 22:15,  1 user,  load average: 0.84, 0.46, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -114,7 +114,7 @@ static bool make_token(char *e) {
           case TK_HNUM:
           //case TK_REG:
              if(substr_len > 32){
-                printf(L_PURPLE "warning: the TK_SUB string is too long [-Woverflow]\n" NONE );
+                printf("warning: the TK_SUB string is too long [-Woverflow]\n");
                 assert(0);
               } else{
                 memset(tokens[nr_token].str, '\0', 32); //important!reset str! or you will get wrong str.
