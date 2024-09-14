c127f6128988cb37aba25366340313b63894520a
tracer-ysyx <tracer@ysyx.org>
2024-09-14 10:41:45 +0800

    > compile NEMU ysyx_24040000 林建辉 Linux HP 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:41:45 up 9 days, 10:54,  1 user,  load average: 0.13, 0.20, 0.31

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -40,6 +40,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i = 0; i < 32; i++){
     if(strcmp(s+1, regs[i]) == 0){
       *success = true;
+      printf("cpu.gpr[%d] = %d\n", i, cpu.gpr[i]);
       return cpu.gpr[i];
     }
   }
