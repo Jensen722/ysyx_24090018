2cf76a6227cedd3c1d7a1b8c629a13bc77b01e28
tracer-ysyx <tracer@ysyx.org>
2024-09-14 11:15:39 +0800

    > compile NEMU ysyx_24040000 林建辉 Linux HP 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:15:39 up 9 days, 11:28,  1 user,  load average: 0.22, 0.26, 0.27

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -18,7 +18,6 @@
 #include <cpu/difftest.h>
 #include <locale.h>
 
-#include "watchpoint.h"
 
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
