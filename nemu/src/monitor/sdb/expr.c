5b753c9a1b773d21f79ba67764d9b64572e18e86
tracer-ysyx <tracer@ysyx.org>
2024-09-20 16:30:11 +0800

    > compile NEMU ysyx_24040000 林建辉 Linux HP 6.8.0-41-generic #41-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  2 20:41:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:30:11 up 15 days, 16:43,  1 user,  load average: 0.36, 0.44, 0.51

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -183,7 +183,7 @@ word_t eval(int p, int q){
      if(success){
        return reg_result;
      }
-     panic(L_RED"reg not exit!\n"NONE);
+     panic(L_RED "reg not exit!\n" NONE);
 
    } else if (check_parentheses(p, q) == true) {
       // The expression is surrounded by a matched pair of parentheses.
