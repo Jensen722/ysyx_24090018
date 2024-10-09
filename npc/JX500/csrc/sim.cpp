4b556e3010ae5ff81d2852b526841ee02116545b
tracer-ysyx <tracer@ysyx.org>
2024-10-09 18:23:53 +0800

    > sim RTL ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:23:53 up 1 day,  3:12,  1 user,  load average: 0.43, 0.24, 0.24

--- npc/JX500/csrc/sim.cpp
+++ npc/JX500/csrc/sim.cpp
@@ -65,7 +65,7 @@ int main() {
 
   while(1) {
     top->inst_i = pmem_read(top->pc_o);
-    printf("inst = %x\n", pmem_read(top->pc_o));
+    printf("inst = 0x%-08x\n", pmem_read(top->pc_o));
     single_cycle();
    //printf("op1 = %d\n", top->op1_o);
   //printf("op2 = %d\n", top->op2_o);
