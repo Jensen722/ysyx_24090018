7c0ed86f219a76d19ec797c9825cceae44a437cc
tracer-ysyx <tracer@ysyx.org>
2024-10-09 18:31:33 +0800

    > sim RTL ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:31:33 up 1 day,  3:19,  1 user,  load average: 0.35, 0.24, 0.24

--- npc/JX500/csrc/sim.cpp
+++ npc/JX500/csrc/sim.cpp
@@ -70,7 +70,7 @@ int main() {
     svBit ebreak;
     svSetScope(svGetScopeFromName("TOP.top.EXU"));
     get_ebreak(&ebreak);
-    if(ebreak) {printf("Finish executing instruction!");break;}
+    if(ebreak) {printf("Finish executing instruction!\n");break;}
   }
 
 
