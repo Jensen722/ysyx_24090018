d958204e32c2558081de5a7cf2034ea0b9691041
tracer-ysyx <tracer@ysyx.org>
2024-10-12 09:30:15 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:30:15 up 3 days, 18:18,  1 user,  load average: 0.85, 0.58, 0.36

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -35,10 +35,9 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
-	$(NEMU_EXEC)
+	@echo 'c' | $(NEMU_EXEC)
 
 #test
-	@echo 'c'
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
