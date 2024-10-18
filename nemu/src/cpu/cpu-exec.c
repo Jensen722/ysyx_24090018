225957396fc03ff79ec3b6e78f720f5fbe0ad952
tracer-ysyx <tracer@ysyx.org>
2024-10-18 16:29:55 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:29:55 up 10 days,  1:18,  1 user,  load average: 0.32, 0.29, 0.27

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -181,8 +181,8 @@ void WriteRingBuff(const char *log_buf){
 }
 
 void ReadRingBuff(){
-  char *p = pRead;
-  printf("R:%s\n", p - 1);
+  //char *p = pRead;
+  printf("R:%s\n", pRead - 1);
   /*
   for(int i = 0; i < BUFF_MAX_LEN; i++){
     printf("%s\n", p); 
