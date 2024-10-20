c8c8b216b4f2ca9c85c47a5d31ea883588b4cbb2
tracer-ysyx <tracer@ysyx.org>
2024-10-20 17:23:58 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:23:58 up 12 days,  2:12,  1 user,  load average: 0.38, 0.32, 0.28

--- nemu/src/utils/iringbuf.c
+++ nemu/src/utils/iringbuf.c
@@ -35,7 +35,7 @@ void inst_ringbuf_record(Decode *s){
   p += space_len; 
 #ifndef CONFIG_ISA_loongarch32r
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
+  disassemble(p, rb.ringbuf[rb.idx] + sizeof(rb.ringbuf[rb.idx]) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
