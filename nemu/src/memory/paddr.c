a99342f6624ed1fdd9699e245a799dc455270071
tracer-ysyx <tracer@ysyx.org>
2024-10-20 19:37:09 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:37:09 up 12 days,  4:25,  1 user,  load average: 0.28, 0.26, 0.27

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -19,6 +19,9 @@
 #include <isa.h>
 #include <utils.h>
 
+#define MTRACE_START 0x80000000
+#define MTRACE_END 0x80000020
+
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
@@ -53,7 +56,9 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
 #ifdef CONFIG_MTRACE
-  printf(ANSI_FMT("[Read ] ", ANSI_FG_GREEN)"Address: 0x%X | Value_Read:  0x%08X\n", addr, pmem_read(addr, len));
+  if(addr > MTRACE_START && addr < MTRACE_END){
+    printf(ANSI_FMT("[Read ] ", ANSI_FG_GREEN)"Address: 0x%X | Value_Read:  0x%08X\n", addr, pmem_read(addr, len));
+  }
 #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -63,7 +68,9 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACE
-  printf(ANSI_FMT("[Write] ", ANSI_FG_BLUE)"Address: 0x%X | Value_Write: 0x%08X\n", addr, data);
+  if(addr > MTRACE_START && addr < MTRACE_END){
+    printf(ANSI_FMT("[Write] ", ANSI_FG_YELLOW)"Address: 0x%X | Value_Write: 0x%08X\n", addr, data);
+  }
 #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
