fccd4c1466b9417be5664ef6f028662004ba22d0
tracer-ysyx <tracer@ysyx.org>
2024-10-20 19:13:51 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:13:51 up 12 days,  4:02,  1 user,  load average: 0.30, 0.26, 0.31

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,6 +51,9 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
+  printf("[Read ] Address: 0x%X | Value_Read: 0x%X", addr, pmem_read(addr, len));
+#endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -58,6 +61,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  printf("[Write] Address: 0x%X | Value_Write: 0x%X", addr, data);
+#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
