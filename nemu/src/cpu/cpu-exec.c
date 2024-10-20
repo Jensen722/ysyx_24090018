9f2e935c5f37d7f8506f755a7f96a9e11f8853b8
tracer-ysyx <tracer@ysyx.org>
2024-10-20 12:05:07 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:05:07 up 11 days, 20:53,  1 user,  load average: 0.36, 0.42, 0.34

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,7 +39,7 @@ bool *scan_wp();
 
 
 typedef struct{
-  char ringbuf[LOGBUF_SIZE][BUFF_MAX_LEN];
+  char ringbuf[BUFF_MAX_LEN][LOGBUF_SIZE];
   int head;
   int tail;
   int wr_idx;
@@ -58,7 +58,7 @@ void WriteRingBuff(char *log_buf, RingBuff *rb){
 printf("mem0:%s\n", rb->ringbuf[rb->wr_idx]);
 memset(rb->ringbuf[rb->wr_idx], 0, sizeof(rb->ringbuf[rb->wr_idx]));
 memcpy(rb->ringbuf[rb->wr_idx], log_buf, 128);
-rb->ringbuf[LOGBUF_SIZE-1][rb->wr_idx] = '\0';
+rb->ringbuf[rb->wr_idx][LOGBUF_SIZE-1] = '\0';
 printf("mem1:%s\n", rb->ringbuf[rb->wr_idx]);
   rb->rd_idx = rb->wr_idx;
   if(rb->wr_idx == rb->tail){
