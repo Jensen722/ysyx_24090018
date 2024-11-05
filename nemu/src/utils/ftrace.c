c4e42fd2202027e52af4e2735d002d9da24a8ed6
tracer-ysyx <tracer@ysyx.org>
2024-11-05 19:11:53 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:11:53 up 28 days,  4:00,  1 user,  load average: 0.72, 0.57, 0.35

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -15,6 +15,10 @@
 
 void init_ftrace(const char *elf_file){
   FILE *fp = fopen(elf_file, "rb");
+if (!fp) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+}
   Assert(fp, "Can not open '%s'", elf_file);
 
   Elf32_Ehdr elf_header;
