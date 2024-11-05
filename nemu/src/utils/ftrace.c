aae6b89165fad9e02ddfce8edaa0bd8681e96b05
tracer-ysyx <tracer@ysyx.org>
2024-11-05 20:17:24 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:17:24 up 28 days,  5:05,  1 user,  load average: 0.64, 0.35, 0.27

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -73,8 +73,9 @@ void init_ftrace(const char *elf_file){
 int num_symbols = symtab_section->sh_size / sizeof(Elf32_Sym);
 printf("entry = %d\n", num_symbols);
     for (int i = 0; i < num_symbols; i++) {
-        if (ELF64_ST_TYPE(symtab[i].st_info) != STT_FILE && symtab[i].st_name != 0) {
-            printf("Symbol: %s, Address: 0x%x, Size: %u\n",
+        if (ELF32_ST_TYPE(symtab[i].st_info) != STT_FILE && symtab[i].st_name != 0) {
+            printf("%d Symbol: %s, Address: 0x%x, Size: %u\n",
+                   i,
                    &strtab[symtab[i].st_name],
                    symtab[i].st_value,
                    symtab[i].st_size);
