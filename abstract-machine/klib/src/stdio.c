ceac1e0f697f8b4605d8bc9633d74535db14cecf
tracer-ysyx <tracer@ysyx.org>
2024-11-11 15:09:15 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:09:15 up 2 days, 22:57,  1 user,  load average: 0.32, 0.37, 0.38

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,11 +6,19 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 int printf(const char *fmt, ...) {
     char buf[64];
-    int buf_len = 0;
+    //int buf_len = 0;
     int len = 0;
     va_list ap;
     va_start(ap, fmt);
 
+
+    len = vsprintf(buf, fmt, ap);
+    for(int i = 0; buf[i] != '\0'; i++){
+      putch(buf[i]);
+    }
+    va_end(ap);
+    return len;
+    /*
     while(*fmt != '\0')
     {
         if(*fmt == '%')
@@ -156,7 +164,7 @@ int printf(const char *fmt, ...) {
         fmt++;
     }
     va_end(ap);
-    return len;
+    return len;*/
 }
 
 
