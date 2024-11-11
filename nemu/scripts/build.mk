9e7fb5c07dd11698326c4585a7e28f55ec69ff7b
tracer-ysyx <tracer@ysyx.org>
2024-11-11 13:11:45 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:11:45 up 2 days, 20:59,  1 user,  load average: 0.48, 0.31, 0.25

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -23,7 +23,7 @@ endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
 CFLAGS  := -O2 -MMD -Wall -Werror -g $(INCLUDES) $(CFLAGS)
-LDFLAGS := -O2 $(LDFLAGS) --no-relax
+LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
