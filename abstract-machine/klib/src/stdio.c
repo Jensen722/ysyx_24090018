f23b667843991eadd1366a64b7839173c34ad1bb
tracer-ysyx <tracer@ysyx.org>
2024-11-11 11:15:12 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:15:12 up 2 days, 19:02,  1 user,  load average: 0.22, 0.28, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -25,7 +25,7 @@ int printf(const char *fmt, ...) {
                         { 
                            putch('-');
                            len++;
-                           assert(num == -2147483648);
+                           assert(num == (int)-2147483648);
                            if(num == -2147483648){
                              putch('2');
                              putch('1');
