3b8cfdb9ac03eef1f87ee86a58be4357ca35b253
tracer-ysyx <tracer@ysyx.org>
2024-11-13 09:53:58 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:53:58 up 4 days, 17:41,  1 user,  load average: 0.08, 0.18, 0.18

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -24,6 +24,7 @@ static int nr_map = 0;
 //给定一个物理地址，返回是属于第几个map的
 static IOMap* fetch_mmio_map(paddr_t addr) {
   int mapid = find_mapid_by_addr(maps, nr_map, addr);
+  printf("nr_map: %d\n", nr_map);
   return (mapid == -1 ? NULL : &maps[mapid]);
 }
 
