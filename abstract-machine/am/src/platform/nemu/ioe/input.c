f4f3611a04607e37146309bdfa6530e0882856b0
tracer-ysyx <tracer@ysyx.org>
2024-11-14 15:34:05 +0800

    完成至PA2.3键盘，能玩mario

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,17 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key = inl(KBD_ADDR);  
+  if(key == AM_KEY_NONE){
+    kbd->keydown = false;
+    kbd->keycode = AM_KEY_NONE;
+  } else{
+    if(key & KEYDOWN_MASK){
+      kbd->keydown = true;
+      kbd->keycode = key & (0x7FFF);
+    } else{
+      kbd->keydown = false;
+      kbd->keycode = key;
+      }
+    }
 }
