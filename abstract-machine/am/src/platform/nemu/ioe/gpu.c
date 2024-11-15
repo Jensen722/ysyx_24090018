cfcc6240e557deb424442f14c3e3ffaaf99cb564
tracer-ysyx <tracer@ysyx.org>
2024-11-15 13:57:01 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:57:01 up 23:46,  1 user,  load average: 0.29, 0.31, 0.20

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,12 +27,23 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  uint32_t *pixels = (uint32_t *)ctl->pixels;
+uint32_t *pixels = (uint32_t *)ctl->pixels;
+  int w = ctl->w, h = ctl->h;
+  int x_start = ctl->x, y_start = ctl->y;
+
+  for (int y = 0; y < h; y++) {
+    for (int x = 0; x < w; x++) {
+      // 计算帧缓冲区中该像素的地址
+      uintptr_t pixel_addr = FB_ADDR + ((y_start + y) * 400 + (x_start + x)) * sizeof(uint32_t);
+      outl(pixel_addr, pixels[y * w + x]);
+    }
+  }
+  /*uint32_t *pixels = (uint32_t *)ctl->pixels;
   int x = ctl->x / ctl->w;
  // int y = ctl->y / ctl->h;
   //printf("ADDR = %x\n", FB_ADDR + 4*x);
   //printf("y = %d\n", y);
-  outl(FB_ADDR + 4*x, *(pixels+x));
+  outl(FB_ADDR + 4*x, *(pixels+x));*/
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
