587481baed034ffaa7071c2abfa94890319690f1
tracer-ysyx <tracer@ysyx.org>
2024-11-15 10:42:23 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:42:23 up 20:31,  1 user,  load average: 0.68, 0.42, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -8,8 +8,8 @@ void __am_gpu_init() {
    uint16_t width = inl(VGACTL_ADDR) >> 16;
    uint16_t height = inl(VGACTL_ADDR);
 
-   int w = width;
-   int h = height;
+   uint32_t w = width / 2;
+   uint32_t h = height / 2;
    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
    for (i = 0; i < w * h; i ++) fb[i] = i;
      outl(SYNC_ADDR, 1);
