7ad373a34cab3ac1584169d2168881585f9de857
tracer-ysyx <tracer@ysyx.org>
2024-11-15 15:40:57 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:40:57 up 1 day,  1:30,  1 user,  load average: 0.61, 0.49, 0.39

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,15 +1,9 @@
 #include <am.h>
 #include <nemu.h>
-#include <klib.h>
+
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  /* int i;
- int w = io_read(AM_GPU_CONFIG).width;
-  int h = io_read(AM_GPU_CONFIG).height;
-   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-   for (i = 0; i < w * h; i ++) fb[i] = i;
-     outl(SYNC_ADDR, 1);*/
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -24,10 +18,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
-uint32_t *pixels = (uint32_t *)ctl->pixels;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
   int w = ctl->w, h = ctl->h;
   int x_start = ctl->x, y_start = ctl->y;
 
@@ -35,15 +26,13 @@ uint32_t *pixels = (uint32_t *)ctl->pixels;
     for (int x = 0; x < w; x++) {
       // 计算帧缓冲区中该像素的地址
       uintptr_t pixel_addr = FB_ADDR + ((y_start + y) * io_read(AM_GPU_CONFIG).width + (x_start + x)) * sizeof(uint32_t);
-      outl(pixel_addr, pixels[y * w + x]);
+      outl(pixel_addr, pixels[y * w + x]); //输出绘制区域的像素
     }
   }
-  /*uint32_t *pixels = (uint32_t *)ctl->pixels;
-  int x = ctl->x / ctl->w;
- // int y = ctl->y / ctl->h;
-  //printf("ADDR = %x\n", FB_ADDR + 4*x);
-  //printf("y = %d\n", y);
-  outl(FB_ADDR + 4*x, *(pixels+x));*/
+
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
