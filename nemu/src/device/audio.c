5e80d8a60836fc8e89a6fd7a5b03cb8c588cb8ea
tracer-ysyx <tracer@ysyx.org>
2024-11-18 15:24:16 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:24:16 up 4 days,  1:13,  1 user,  load average: 0.85, 1.00, 0.75

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -81,7 +81,7 @@ SDL_PauseAudio(0);
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   if(!is_write){
     audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
-    audio_base[reg_count] = audio_wr_ptr;
+    audio_base[reg_count] = audio_wr_ptr - audio_pos;
   } 
 }
 
