300a596ad3165cafdd025345db6712521aa462ec
tracer-ysyx <tracer@ysyx.org>
2024-11-18 10:44:12 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:44:12 up 3 days, 20:33,  1 user,  load average: 0.45, 0.38, 0.36

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -34,12 +34,12 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   int len = ctl->buf.end - ctl->buf.start;
 
   printf("count = %d, buf_size= %d, len = %d\n", count, bufsize, len);
-  //uint8_t *buf_start = (uint8_t *)ctl->buf.start;
+  uint8_t *buf_start = (uint8_t *)ctl->buf.start;
   while(bufsize - count < len); //若当前流缓冲区的空闲空间少于即将写入的音频数据, 此次写入将会一直等待, 直到有足够的空闲空间
 
   for(int i = 0; i < len; i++){
-    //outb(audio_start_adddr + i, *(buf_start + i));
-    outb(audio_start_adddr + i, 'a');
+    outb(audio_start_adddr + i, *(buf_start + i));
+    //outb(audio_start_adddr + i, 'a');
   }
   audio_start_adddr = (audio_start_adddr + len);  //环形缓冲区
                                                   printf("start adr= 0x%x\n", audio_start_adddr);
