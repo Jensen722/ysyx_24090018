492841bd51b427f415eea5a6b47d971d4a90aa80
tracer-ysyx <tracer@ysyx.org>
2024-11-19 16:51:58 +0800

    > compile NEMU ysyx_24090018 林建辉 Linux HP 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:51:58 up 5 days,  2:41,  1 user,  load average: 0.20, 0.26, 0.39

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -27,21 +27,19 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
   stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
-uint32_t am_audio_pos = 0;
+uint32_t am_audio_len = 0;
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   //int count = io_read(AM_AUDIO_STATUS).count;
   int bufsize = io_read(AM_AUDIO_CONFIG).bufsize;
   int len = ctl->buf.end - ctl->buf.start;
 
   uint8_t *buf_start = (uint8_t *)ctl->buf.start;
-  //printf("count = %d, buf_start=0x%x\n", count, buf_start);
-  //if(len == 0) {return;}
   //while(bufsize - count >= len){ //若当前流缓冲区的空闲空间少于即将写入的音频数据, 此次写入将会一直等待, 直到有足够的空闲空间
 
   for(int i = 0; i < len; i++){
-    outb(AUDIO_SBUF_ADDR + (am_audio_pos + i)%bufsize, *(buf_start + i));
-    //printf("addr = 0x%x, buf = %x\n", AUDIO_SBUF_ADDR + am_audio_pos + i,*(buf_start + i));
+    uint32_t audio_waddr = AUDIO_SBUF_ADDR + (am_audio_len + i) % bufsize;
+    outb(audio_waddr, *(buf_start + i));
   }
   //}
-  am_audio_pos = (am_audio_pos + len) % bufsize;  //环形缓冲区
+  am_audio_len = am_audio_len + len;
 }
