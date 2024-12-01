a144f5a56c146cea1eae36d0a85bd5a4efd93e29
tracer-ysyx <tracer@ysyx.org>
2024-12-01 20:16:23 +0800

    将JX500的文件转移至npc中

---
+++ npc/csrc/sim.cpp
@@ -0,0 +1,80 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+#include <time.h>
+#include "Vtop__Dpi.h"
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vtop* top;
+
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vtop;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("wave.vcd");
+}
+
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+}
+static void single_cycle() {
+  top->clk = 0; step_and_dump_wave();
+  top->clk = 1; step_and_dump_wave();
+}
+
+static void reset(int n) {
+  top->rst = 0;
+  while (n -- > 0) single_cycle();
+  top->rst = 1;
+}
+
+
+static uint32_t inst_mem[1000] = {}; 
+static void put_inst(){
+  inst_mem[0] = 0x00500093; //addi x1, x0, 5
+  inst_mem[1] = 0x00a08113; //addi x2, x1, 10
+  inst_mem[2] = 0x2b878793; //addi x15, x15, 696
+  inst_mem[3] = 0x00710293; //addi x5, x3, 7
+  inst_mem[4] = 0xff410113; //addi sp, sp, -12
+  inst_mem[5] = 0xfe010113; //addi sp, sp, -32
+  inst_mem[6] = 0x01c50513; //addi a0, a0, 28
+  inst_mem[7] = 0x00848493; //addi s1, s1, 8
+  inst_mem[8] = 0x00100073; //ebreak
+}
+static uint32_t pmem_read(uint32_t raddr){
+  int idx = (raddr - 0x80000000) / 4;
+  return inst_mem[idx];
+}
+int main() {
+  sim_init();
+
+  put_inst();
+  int inst_num = 0;
+  reset(1); 
+
+  while(1) {
+    top->inst_i = pmem_read(top->pc_o);
+    printf("inst = 0x%08x\n", pmem_read(top->pc_o));
+    single_cycle();
+
+    svBit ebreak;
+    svSetScope(svGetScopeFromName("TOP.top.EXU"));
+    get_ebreak(&ebreak);
+    if(ebreak) {printf("Finish executing instruction!\n");break;}
+  }
+
+
+  sim_exit();
+}
+
+
---
+++ npc/vsrc/EXU.v
@@ -0,0 +1,61 @@
+/*************************************************************************
+	> File Name: EXU.v
+	> Author: Jensen
+	> Mail: 1624839259@qq.com 
+	> Created Time: 2024年10月07日 星期一 13时57分16秒
+ ************************************************************************/
+`include "Defines.v"
+
+module ysyx_24090018_EXU #(DATA_WIDTH = 32)(
+  input [DATA_WIDTH-1:0] op1_i,
+  input [DATA_WIDTH-1:0] op2_i,
+  input [DATA_WIDTH-1:0] inst_i,
+
+  output reg [DATA_WIDTH-1:0] rf_wdata_o
+);
+  wire [6:0] opcode;
+  wire [2:0] funct3;
+  wire [6:0] funct7;
+
+  assign opcode = inst_i[6:0];
+  assign funct3 = inst_i[14:12];
+  assign funct7 = inst_i[31:25];
+
+  reg ebreak;
+  export "DPI-C" function get_ebreak;
+  function get_ebreak;
+    output bit finish;
+    finish = ebreak;
+  endfunction
+
+  always@(*) begin
+    case(opcode)
+      `ysyx_24090018_INST_I_0010011: begin
+        case(funct3) 
+          `ysyx_24090018_funct3_ADDI: begin
+             rf_wdata_o = op1_i + op2_i;
+          end
+          default: begin
+             rf_wdata_o = `ysyx_24090018_ZeroWord;
+          end
+        endcase
+      end
+      `ysyx_24090018_INST_I_1110011: begin
+          case(funct3)
+            `ysyx_24090018_funct3_EBREAK_ECALL: begin
+                ebreak = inst_i[20] ? 1'b1 : 1'b0;
+                rf_wdata_o = `ysyx_24090018_ZeroWord; 
+            end
+            default: begin
+                ebreak = 1'b0;
+                rf_wdata_o = `ysyx_24090018_ZeroWord;
+             end
+          endcase
+
+      end
+      default: begin
+        rf_wdata_o = `ysyx_24090018_ZeroWord; 
+      end
+    endcase
+  end
+endmodule
