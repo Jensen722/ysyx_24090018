eb9fe448b3ea7311d006265584b41a0532b98537
tracer-ysyx <tracer@ysyx.org>
2024-12-02 15:27:13 +0800

    > sim RTL ysyx_24090018 林建辉 Linux HP 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:27:13 up 2 days, 19:49,  1 user,  load average: 0.38, 0.45, 0.40

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -8,6 +8,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
+#include<stdint.h>
 
 #define CONFIG_MSIZE 0x8000000
 #define CONFIG_MBASE 0x80000000
@@ -17,8 +18,8 @@ static inline int host_read(void *addr){
   return *(int *)addr;
 }
 
-static int *pmem = NULL;
-int* guest_to_host(int addr) { return pmem + addr - CONFIG_MBASE; }
+static uint8_t *pmem = NULL;
+uint8_t* guest_to_host(int addr) { return pmem + addr - CONFIG_MBASE; }
 int pmem_read(int raddr){
   int aligned_addr = raddr & ~0x3u;
   int ret = host_read(guest_to_host(aligned_addr));
@@ -29,7 +30,7 @@ void pmem_write(int waddr, int wdata, char wmask){
 }
 
 void init_mem() {
-  pmem = (int *)malloc(CONFIG_MSIZE);
+  pmem = (uint8_t *)malloc(CONFIG_MSIZE);
   assert(pmem);
 }
 
